4b7de0bca5d1fa652c68f9f8b45098ba6ffaa485
tdtcs <fhollis983@gmail.com>
2023-04-12 01:48:02 -0700

    added sudo

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pacman -Syy
-pacman -S screen vim vi nano htop neofetch
+sudo pacman -Syy
+sudo pacman -S screen vim vi nano htop neofetch
 
-pacman -S git base-devel
+sudo pacman -S git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
