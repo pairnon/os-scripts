fb653450e80302a67a326bc67ac124c1637f9b73
tdtcs <fhollis983@gmail.com>
2023-04-12 20:49:35 -0700

    added gui package prompt

--- arch/setup.sh
+++ arch/setup.sh
@@ -6,6 +6,24 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+# Prompt for GUI package installation
+valid=false
+gui_packages=false
+while [ "$valid" = false ]; do
+	echo "Install GUI packages? [y/N] "
+	read choice
+	if [ "$choice" == "y" ]; then
+		gui_packages=true
+		valid=true
+	elif [ "$choice" == "n" ]; then	
+		valid=true
+	elif [ "$choice" == "" ]; then
+		valid=true
+	else
+		echo "Invalid choice. Please enter y or n."
+	fi
+done
+
 pacman -Syy
 pacman -S sudo screen vim vi nano htop neofetch
 
