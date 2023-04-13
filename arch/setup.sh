98d4a60c421f6b05f378b5bfa6d57894f38ee6d1
tdtcs <fhollis983@gmail.com>
2023-04-12 20:39:22 -0700

    added sudo

--- arch/setup.sh
+++ arch/setup.sh
@@ -2,12 +2,12 @@
 
 # Check if script is being run as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
+	echo "This script must be run as root"
+	exit 1
 fi
 
 pacman -Syy
-pacman -S screen vim vi nano htop neofetch
+pacman -S sudo screen vim vi nano htop neofetch
 
 pacman -S git base-devel
 git clone https://aur.archlinux.org/yay.git
